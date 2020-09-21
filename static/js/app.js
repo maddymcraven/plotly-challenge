a6eed2c84aa358f3ca672959b0b7ee3c2bff8e85
maddymcraven <maddymcraven@gmail.com>
2020-09-21 18:21:49 -0500

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,52 +1,71 @@
-  d3.json("samples.json").then((importedData) => {
-    var data = importedData;
-    var names = data.names;
-    var samples = data.samples;
-    var metadata = data.metadata;
+// updated!!!!! 
 
-    function init(){
-        updateBar(941);
-    };
-    
-    function updateBar(sample_id){
+// Fetch the JSON data and console log it
+d3.json("samples.json").then(function (data) {
+    var samples = data.samples
+    // var sample_values = []
+    // var otu_labels = []
+    // var otu_ids = []
 
-        for(var i=0;i<samples.length;i++) {
-            console.log(samples[i].id)
-            // if(samples.id[i] == 941)
-            //     sample_values = samples[i].sample_values;
-            //     otu_labels = samples[i].otu_labels;
-            //     otu_ids = samples[i].otu_ids;
-            //     console.log(samples.id[i]);
-        };
-        
-    
-    //     var trace1 = {
-    //         x: sample_values,
-    //         y: otu_ids,
-    //         text: otu_labels,
-    //         type: "bar",
-    //         orientation: "h"
-    //       };
-        
-    //       // data
-    //       var chartData = [trace1];
-        
-    //       // Apply the group bar mode to the layout
-    //     //   var layout = {
-    //     //     title: "Greek gods search results",
-    //     //     margin: {
-    //     //       l: 100,
-    //     //       r: 100,
-    //     //       t: 100,
-    //     //       b: 100
-    //     //     }
-    //     //   };
-        
-    //     // Render the plot to the div tag with id "plot"
-    //     Plotly.newPlot("sample-metadata", chartData);    
+    // for (var i = 0; i < samples.length; i++) {
+    //     sample_values.push(samples[i].sample_values)
+    //     otu_labels.push(samples[i].otu_labels)
+    //     otu_ids.push(samples[i].otu_ids)
     // };
-    
-    init();
-    };
 
-};
+    // var number = 2
+    // console.log(sample_values[number])
+    // console.log(otu_labels[number])
+    // console.log(otu_ids[number])
+
+    // console.log(samples)
+
+    // this needs to be the interactive part
+    var number = "941"
+
+    for (var i = 0; i < samples.length; i++) {
+        if (samples[i].id === number) {
+            sample_values = samples[i].sample_values;
+            otu_labels = samples[i].otu_labels;
+            otu_ids = samples[i].otu_ids;
+        }
+    }
+    console.log(sample_values)
+    console.log(otu_labels)
+    console.log(otu_ids)
+    
+    // graphing 
+    var trace1 = {
+        y: sample_values,
+        x: otu_ids,
+        text: otu_labels,
+        type: "bar",
+        orientation: 'h'
+    };
+    
+    var data = [trace1];
+    
+    var layout = {
+        title: "'Bar' Chart",
+        height: 400
+    };
+    
+    Plotly.newPlot("sample-metadata", data, layout);
+});
+
+// Promise Pending
+// const dataPromise = d3.json(url);
+// console.log("Data Promise: ", dataPromise);
+
+
+// for(var i=0;i<samples.length;i++) {
+//     console.log(samples[i].id)
+//     // if(samples.id[i] == 941)
+//     //     sample_values = samples[i].sample_values;
+//     //     otu_labels = samples[i].otu_labels;
+//     //     otu_ids = samples[i].otu_ids;
+//     //     console.log(samples.id[i]);
+// };
+
+
+
